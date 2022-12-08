e6b85b8033dfde744363072bcf4dd44457e4b0ee
Timar-Mara-Iunia <tmaraiunia@yahoo.com>
2022-12-08 13:57:03 +0200

    Login Controller

---
+++ src/main/java/com/example/iocteatime/controllersGUI/LoginController.java
@@ -0,0 +1,17 @@
+package com.example.iocteatime.controllersGUI;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class LoginController {
+
+    private TextField usernameField;
+    private PasswordField passwordField;
+    private Button logInButton;
+    private Button registerButton;
+    private ImageView imageView;
+
+
+}
